0b989a10ca7b0daaa62b6bb2775151c7d60773cd
Dimantrie <tankerlord@mail.ru>
2022-08-20 20:45:28 +0300

    sign in+

--- src/components/SignInForm/SignInForm.tsx
+++ src/components/SignInForm/SignInForm.tsx
@@ -1,29 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./SignInForm.scss";
 
 const SignInForm = () => {
   return (
-    <div className="SignUp-container">
-      {/* <h2>Create new account</h2>
+    <div className="SignIn-container">
+      <h2>Sign In</h2>
       <form>
         <label>
-          Username:
-          <input type="text" name="username" placeholder="Username" />
+          Email address
+          <input type="text" name="email" placeholder="Email address" />
         </label>
-
-        <label>Email address</label>
-        <input type="text" name="email" placeholder="Email address" />
-        <label>Password</label>
-        <input type="text" name="password" placeholder="Password" />
-        <label>Repeat Password</label>
-        <input type="text" name="passwordRepeat" placeholder="Password" />
+        <label style={{ marginBottom: "21px" }}>
+          Password
+          <input type="text" name="password" placeholder="Password" />
+        </label>
       </form>
-      <button className="sign-up-submit">Create</button>
+      <button className="sign-up-submit">Login</button>
       <div className="sign-up-refuse">
-        Already have an account? <Link to="/sign-in"> Sign In.</Link>
-      </div> */}
-      11
+        Already have an account? <Link to="/sign-up"> Sign Up.</Link>
+      </div>
     </div>
   );
 };
